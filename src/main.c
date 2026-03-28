72e003ea350a36da40df574ab75f4ccb750e0040
Andreas Viks <117015417+anviks@users.noreply.github.com>
2026-03-28 13:33:09 +0200

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -3,7 +3,7 @@
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
-  setbuf(stdout, NULL);
+  // setbuf(stdout, NULL);
 
   // TODO: Uncomment the code below to pass the first stage
   printf("$ ");
