6a48015837a0ca768436e69a9573df6ed28b83e1
Andreas Viks <117015417+anviks@users.noreply.github.com>
2026-03-28 13:43:46 +0200

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, char *argv[]) {
-  // Flush after every printf
-  setbuf(stdout, NULL);
+int main(int argc, char* argv[]) {
+    // Flush after every printf
+    setbuf(stdout, NULL);
 
-  printf("$ ");
-  char command[1024];
-  fgets(command, sizeof(command), stdin);
-  command[strlen(command) - 1] = '\0';
+    while (1) {
+        printf("$ ");
+        char command[1024];
+        fgets(command, sizeof(command), stdin);
+        command[strlen(command) - 1] = '\0';
 
-  printf("%s: command not found\n", command);
+        printf("%s: command not found\n", command);
+    }
 
-  return 0;
+    return 0;
 }
