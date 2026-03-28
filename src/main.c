56a20b434550a24d768eaec407daee9980c46360
Andreas Viks <117015417+anviks@users.noreply.github.com>
2026-03-28 13:56:02 +0200

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -11,9 +11,14 @@ int main(int argc, char* argv[]) {
         fgets(command, sizeof(command), stdin);
         command[strlen(command) - 1] = '\0';
 
+        if (strcmp(command, "") == 0) continue;
         if (strcmp(command, "exit") == 0) break;
-
-        printf("%s: command not found\n", command);
+        
+        if (strncmp(command, "echo ", 5) == 0) {
+            printf("%s\n", command + 5);
+        } else {
+            printf("%s: command not found\n", command);
+        }
     }
 
     return 0;
