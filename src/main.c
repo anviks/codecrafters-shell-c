e842631031bfe6b9dac2a33c8d129676f9f72da1
Andreas Viks <117015417+anviks@users.noreply.github.com>
2026-03-28 13:35:36 +0200

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -6,7 +6,6 @@ int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  // TODO: Uncomment the code below to pass the first stage
   printf("$ ");
   char command[1024];
   fgets(command, sizeof(command), stdin);
