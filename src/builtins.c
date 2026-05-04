58717d9686b4ef91ca374b35bdd26ba7191245c7
Andreas Viks <117015417+anviks@users.noreply.github.com>
2026-05-04 23:01:12 +0300

    codecrafters submit [skip ci]

--- src/builtins.c
+++ src/builtins.c
@@ -97,9 +97,13 @@ static void handle_declare(char** argv) {
         if (!variable) fprintf(stderr, "declare: %s: not found\n", argv[2]);
         else printf("declare -- %s=\"%s\"\n", variable->name, variable->value);
     } else {
+        char* eq = strchr(argv[1], '=');
+        char* name = strndup(argv[1], eq - argv[1]);
+        char* value = strdup(eq + 1);
+
         int invalid = 0;
-        if (isdigit(argv[1][0])) invalid = 1;
-        for (char* p = argv[1]; *p; p++) {
+        if (isdigit(name[0])) invalid = 1;
+        for (char* p = name; *p; p++) {
             if (!isalnum(*p) && *p != '_') {
                 invalid = 1;
                 break;
@@ -110,9 +114,6 @@ static void handle_declare(char** argv) {
             return;
         }
 
-        char* eq = strchr(argv[1], '=');
-        char* name = strndup(argv[1], eq - argv[1]);
-        char* value = strdup(eq + 1);
         array_remove(&variables, name);
         array_add(&variables, &(Variable){.name = name, .value = value});
     }
